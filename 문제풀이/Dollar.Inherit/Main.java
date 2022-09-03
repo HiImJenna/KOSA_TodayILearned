3e06413ceebab6b99ed84f45602128c4f74ac78c
HiImJenna <skygarden316@naver.com>
2022-09-04 01:32:36 +0900

    22.09.04.01:32

--- 문제풀이/Dollar.Inherit/Main.java
+++ 문제풀이/Dollar.Inherit/Main.java
@@ -5,7 +5,7 @@ abstract class Converter{
     abstract protected String getSrcString(); // 추상 메소드
     abstract protected String getDestString(); // 추상 메소드
     protected double ratio; // 비율
-
+ 
     public void run() {
         Scanner sc = new Scanner(System.in);
         System.out.println(getSrcString() + "을 " +getDestString() + "로 바꿉니다.");
