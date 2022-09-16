900921264082199c8689198a178130d58f07dd19
HiImJenna <jeongwson@gmail.com>
2022-09-16 15:32:28 +0900

    22.09.16

---
+++ 22.09.16_DAY19/BookManager_Object/Book_Main.java
@@ -0,0 +1,29 @@
+package BookManager_Object;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+public class Book_Main {
+	public static void main(String[] args) {
+		BookManager bm = new BookManager();
+        bm.Run();
+	}
+
+    public static void Read(){
+        String filename = "BookManager.txt";
+
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+        ObjectOutputStream out = null;
+
+        try {
+            fos = new FileOutputStream(filename, true);
+            bos = new BufferedOutputStream(fos);
+            out = new ObjectOutputStream(bos);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
